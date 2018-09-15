2a9204269f6dca7f0c3b058ced3b5979b44f178c
LiadOwaR <liadowar@gmail.com>
2018-09-15 14:13:39 +0300

    improving angular part

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -12,25 +12,14 @@ export class AppComponent {
   title = 'app';
   readonly ROOT_URL = 'http://localhost:8080';
 
-  turn : any;
-  originPos : any;
-  targetPos : any;
-  posts: any;
-  line1: any;
-  line2: any;
-  line3: any;
-  line4: any;
-  line5: any;
-  line6: any;
-  line7: any;
-  line8: any;
+  turn: any;
+  originPos: any;
+  targetPos: any;
   lines: any;
-
-  resp: any;
 
   constructor (private http: HttpClient) {}
 
-  getTurn(){
+  getTurn() {
     this.turn = this.http.get(this.ROOT_URL + '/game/turn', {observe: 'response'})
     .subscribe(resp => {
                           this.turn = resp.body;
@@ -39,20 +28,20 @@ export class AppComponent {
                          });
   }
 
-  startGame(){
-      this.http.get(this.ROOT_URL + '/game/start',{observe: 'response'})
+  startGame() {
+      this.http.get(this.ROOT_URL + '/game/start', {observe: 'response'})
       .subscribe(resp => {this.getAll();
                                 this.getTurn();
 
                                });
     }
   movement(post) {
-    console.log(post.position.position.xPos + "," + post.position.position.yPos);
+    console.log(post.position.position.xPos + ',' + post.position.position.yPos);
     if (this.originPos == null) {
-     this.originPos = post.position.position.xPos + "," + post.position.position.yPos;
+     this.originPos = post.position.position.xPos + ',' + post.position.position.yPos;
     } else {
-      this.targetPos = post.position.position.xPos + "," + post.position.position.yPos;
-      this.http.post(this.ROOT_URL + '/game/move',{
+      this.targetPos = post.position.position.xPos + ',' + post.position.position.yPos;
+      this.http.post(this.ROOT_URL + '/game/move', {
         startPosition : this.originPos,
         destPosition : this.targetPos
       }).subscribe( res => {
@@ -73,111 +62,11 @@ export class AppComponent {
         }
         this.lines = lineArray;
         return this.lines;
-
-     }
+     });
     }
 
 
-  getLine1() {
-   var line1Array = [];
-   this.line1 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-   for (var i = 0 ; i < 8 ; i++){
-    line1Array[i] = (res[i]);
-   }
-   this.line1 = line1Array;
-   return this.line1;
-   }) ;
-  }
-
-    getLine2() {
-     var line2Array = [];
-     this.line2 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-     for (var i = 0 ; i < 8 ; i++){
-      line2Array[i] = (res[i + 8]);
-     }
-     this.line2 = line2Array;
-     return this.line2;
-     }) ;
-    }
-
-    getLine3() {
-     var line3Array = [];
-     this.line3 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-     for (var i = 0 ; i < 8 ; i++){
-      line3Array[i] = (res[i + 16]);
-     }
-     this.line3 = line3Array;
-     return this.line3;
-     }) ;
-    }
-
-    getLine4() {
-     var line4Array = [];
-     this.line4 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-     for (var i = 0 ; i < 8 ; i++){
-      line4Array[i] = (res[i + 24]);
-     }
-     this.line4 = line4Array;
-     return this.line4;
-     }) ;
-    }
-
-   getLine5() {
-       var line5Array = [];
-       this.line5 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-       for (var i = 0 ; i < 8 ; i++){
-        line5Array[i] = (res[i + 32]);
-       }
-       this.line5 = line5Array;
-       return this.line5;
-       }) ;
-      }
-
-   getLine6() {
-         var line6Array = [];
-         this.line6 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-         for (var i = 0 ; i < 8 ; i++){
-          line6Array[i] = (res[i + 40]);
-         }
-         this.line6 = line6Array;
-         return this.line6;
-         }) ;
-        }
-
- getLine7() {
-         var line7Array = [];
-         this.line7 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-         for (var i = 0 ; i < 8 ; i++){
-          line7Array[i] = (res[i + 48]);
-         }
-         this.line7 = line7Array;
-         return this.line7;
-         }) ;
-        }
-
-
- getLine8() {
-         var line8Array = [];
-         this.line8 = this.http.get(this.ROOT_URL + '/game').map(function(res) {
-         for (var i = 0 ; i < 8 ; i++){
-          line8Array[i] = (res[i + 56]);
-         }
-         this.line8 = line8Array;
-         return this.line8;
-         }) ;
-        }
-
-  getPosts() {
-    this.getLine1();
-    this.getLine2();
-    this.getLine3();
-    this.getLine4();
-    this.getLine5();
-    this.getLine6();
-    this.getLine7();
-    this.getLine8();
-  }
-   getLines() {
+    getLines() {
       this.getAll();
     }
 
--- src/main/frontend/src/app/tile/tile.component.ts
+++ src/main/frontend/src/app/tile/tile.component.ts
@@ -1,35 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.css']
 })
-export class TileComponent implements OnInit {
-  result2: any;
-  result: any;
-  posts: any;
-  title: any;
-  url: any;
-  url1: any;
+export class TileComponent {
+  readonly ROOT_URL = 'http://localhost:8080';
+
+  turn: any;
+  originPos: any;
+  targetPos: any;
+  lines = [];
+  tiles = [] ;
+
   constructor(private http: HttpClient) {
-   this.title = "tile title1";
-   this.url = 'http://localhost:8080/game';
-    this.url1 = 'src/app/tile/ex.json';
+    // this.tiles = ['white', 'black'];
+    let i: number;
+    let startLineColor = 'white';
+    this.tiles[0] = startLineColor;
+    for (i = 1; i < 64 ; i++) {
+      let prevTileColor = this.tiles[i - 1];
+      let currentTileColor = this.tiles[i];
+      if (i % 8 === 0 && i !== 0) {
+        currentTileColor = prevTileColor;
+        this.tiles[i] = currentTileColor;
+        i++;
+      }
+      if (prevTileColor === 'black') {
+        currentTileColor = 'white';
+      } else
+      if (prevTileColor === 'white') {
+        currentTileColor = 'black';
+      }
+      this.tiles[i] = currentTileColor;
 
-   console.log(this.url);
-   this.result = http.get(this.url);
-   console.log("result1 " + this.result);
-   this.result2 = http.get(this.url);
-   console.log("result2 " + this.result2);
+    }
+
+
   }
-getResult() {
-        this.posts = this.http.get(this.url +'/posts');
-        this.result = this.http.get(this.url);
-     }
-  ngOnInit() {
+  getTurn() {
+    this.turn = this.http.get(this.ROOT_URL + '/game/turn', {observe: 'response'})
+      .subscribe(resp => {
+        this.turn = resp.body;
+        console.log(this.turn);
+        return this.turn;
+      });
   }
 
+  startGame() {
+    this.http.get(this.ROOT_URL + '/game/start', {observe: 'response'})
+      .subscribe(resp => {this.getAll();
+        this.getTurn();
+      });
+  }
+  movement(post) {
+    console.log(post.position.position.xPos + ',' + post.position.position.yPos);
+    if (this.originPos == null) {
+      this.originPos = post.position.position.xPos + ',' + post.position.position.yPos;
+    } else {
+      this.targetPos = post.position.position.xPos + ',' + post.position.position.yPos;
+      this.http.post(this.ROOT_URL + '/game/move', {
+        startPosition : this.originPos,
+        destPosition : this.targetPos
+      }).subscribe( res => {
+
+        this.getAll();
+        this.getTurn();
+      });
+      this.originPos = null;
+      this.targetPos = null;
+
+    }
+  }
+  getAll() {
+    this.http.get(this.ROOT_URL + '/game', {observe: 'response'}).subscribe(resp => {
+      for (let i = 0 ; i < 64 ; i++) {
+        this.lines[i] = resp.body[i];
+      }
+    });
+  }
+
+
+  getLines() {
+    this.getAll();
+  }
 
 }
